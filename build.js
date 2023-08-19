a849729f5f0861ef171c67eb62acaafb0d25b79f
Wenqi He <hewenqi96@gmail.com>
2023-08-18 22:12:21 -0500

    update

--- build.js
+++ build.js
@@ -12,37 +12,48 @@ async function getUser() {
       "X-GitHub-Api-Version": "2022-11-28",
     },
   });
-  if (res.status == 200) {
-    return res.data;
-  }
-  return null;
+  return res.status == 200 ? res.data : null;
 }
 
-(async () => {
-  const user = await getUser();
-
-  process.stdout.write("<h1>Wenqi He</h1>\n");
+async function getRepos() {
   const res = await octokit.request("GET /user/repos", {
     headers: {
       "X-GitHub-Api-Version": "2022-11-28",
     },
   });
 
-  if (res.status == 200) {
-    process.stdout.write("<ul>");
-    for (const repo of res.data) {
-      if (repo.has_pages && !exclude.has(repo.name)) {
-        if (repo.owner.id == user?.id) {
-          process.stdout.write(
-            `<li><a href="https://galmungral.github.io/${repo.name}"><code>/${
-              repo.name
-            }</code></a> ${
-              repo.description ? `(${repo.description})` : ""
-            }</li>\n`
-          );
-        }
+  return res.status == 200 ? res.data : [];
+}
+
+(async () => {
+  const user = await getUser();
+
+  process.stdout.write(
+    "<h1>Wenqi He</h1>\n" +
+      '<h2><a href="./resume.pdf">Resume</a></h2>\n' +
+      "<b>National Center for Supercomputing Applications</b>, Urbana, IL, '22-'23<br>\n" +
+      "<i>Graduate Research Assistant</i><br>\n" +
+      "<b>University of Illinois Urbana-Champaign</b>, Urbana-Champaign, IL, '22-'23<br>\n" +
+      "<i>Master of Computer Science</i><br>\n" +
+      "<b>Georgia Institute of Technology</b>, Atlanta, GA, '15-'19<br>\n" +
+      '<i>Bachelor of Science in Computer Science, Minor in <a href="./academic/GT-PHYS-7125-Paper.pdf">Physics</a></i><br>\n'
+  );
+
+  const repos = await getRepos();
+  repos.sort((a, b) => (a.created_at > b.created_at ? -1 : 1));
+
+  process.stdout.write("<h2>Demos</h2>\n");
+  process.stdout.write("<ul>");
+  for (const repo of repos) {
+    if (repo.has_pages && !exclude.has(repo.name)) {
+      if (repo.owner.id == user?.id) {
+        process.stdout.write(
+          `<li><a href="https://galmungral.github.io/${repo.name}"><code>${
+            repo.name
+          }</code></a>${repo.description ? ": " + repo.description : ""}</li>\n`
+        );
       }
     }
-    process.stdout.write("</ul>");
   }
+  process.stdout.write("</ul>");
 })();
